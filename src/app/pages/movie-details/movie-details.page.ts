33ea49d8bec286f854d8ef69a3a660a0516d4c02
吳弦承 <wuxiancheng@HCWs-MBP.Home>
2019-02-06 13:00:28 +0800

    add service and movie page

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MovieService } from "./../../services/movie.service";
+
+@Component({
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.page.html",
+  styleUrls: ["./movie-details.page.scss"]
+})
+export class MovieDetailsPage implements OnInit {
+  information = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get("id");
+
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
+  }
+
+  openWebSite() {
+    window.open(this.information.WebSite, "_blank");
+  }
+}
